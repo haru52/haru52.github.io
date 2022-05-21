b22d0b8cf24a700048abec268c2d2bfe9137d492
haru <haru@haru52.com>
2022-05-21 21:37:39 +0900

    perf: reduce bundled Font Awesome SVG scripts

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,4 +1,6 @@
-import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/js/brands';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
 import 'bootstrap';
 import Main from './main';
 import '../assets/stylesheets/index.scss';
